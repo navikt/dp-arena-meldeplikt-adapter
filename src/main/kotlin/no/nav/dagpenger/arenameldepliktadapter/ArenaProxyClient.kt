0c3fcb91ddbe03bf4a8de8977492abaa4ac19944
Igor Weber <85676385+igorweber@users.noreply.github.com>
2026-06-18 14:11:40 +0200

    Logging ved henting av innsendte meldekort + Teams Logs oppsett (#295)

--- src/main/kotlin/no/nav/dagpenger/arenameldepliktadapter/ArenaProxyClient.kt
+++ src/main/kotlin/no/nav/dagpenger/arenameldepliktadapter/ArenaProxyClient.kt
@@ -10,18 +10,19 @@ import io.ktor.http.HttpHeaders
 import mu.KotlinLogging
 import no.nav.dagpenger.arenameldepliktadapter.models.ArenaMeldekort
 import no.nav.dagpenger.arenameldepliktadapter.models.DatadelingRequest
+import no.nav.dagpenger.arenameldepliktadapter.utils.Sikkerlogg
 import no.nav.dagpenger.arenameldepliktadapter.utils.defaultObjectMapper
 import no.nav.dagpenger.arenameldepliktadapter.utils.getEnv
 import no.nav.dagpenger.arenameldepliktadapter.utils.hentAzureToken
 
 private val logger = KotlinLogging.logger {}
-private val sikkerlogger = KotlinLogging.logger("tjenestekall")
 
 class ArenaProxyClient(
     private val httpClient: HttpClient,
 ) {
     suspend fun hentInnsendteMeldekort(request: DatadelingRequest): List<ArenaMeldekort> {
         logger.info("Henter innsendte meldekort fra dp-proxy")
+        Sikkerlogg.info { "Henter innsendte meldekort fra dp-proxy med request: $request" }
 
         val dpProxyBaseUrl = getEnv("DP_PROXY_URL") ?: throw RuntimeException("DP_PROXY_URL er ikke satt")
         val urlString = "$dpProxyBaseUrl/proxy/v1/arena/innsendtemeldekort"
@@ -40,9 +41,13 @@ class ArenaProxyClient(
             if (response.status.value !in 200..299) {
                 throw RuntimeException("dp-proxy svarte ${response.status.value} ${response.status.description}")
             }
-            defaultObjectMapper.readValue<List<ArenaMeldekort>>(response.bodyAsText())
+
+            val responseBody = response.bodyAsText()
+            Sikkerlogg.info { "Hentet innsendte meldekort fra dp-proxy: $responseBody" }
+
+            defaultObjectMapper.readValue<List<ArenaMeldekort>>(responseBody)
         }.getOrElse {
-            sikkerlogger.error(it) { "Kunne ikke hente innsendte meldekort fra url: $urlString for request $request" }
+            Sikkerlogg.error(it) { "Kunne ikke hente innsendte meldekort fra url: $urlString for request $request" }
             throw it
         }
     }
